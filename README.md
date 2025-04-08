d2a23dd3b5028a2e6f949a59bd7011bef5b5ee27
subodhbarcheclover <subodh.barche@cloverinfotech.com>
2025-04-08 11:49:01 +0530

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## ICICI Integration
+
+Integration
+
+#### License
+
+MIT
---
+++ icici_integration/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "ICICI Integration",
+			"type": "module",
+			"label": _("ICICI Integration")
+		}
+	]
